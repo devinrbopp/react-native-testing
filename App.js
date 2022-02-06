7e9168a77f3b645c3a3bd2151a507d7a6fc4f433
devinrbopp <devinrbopp@gmail.com>
2022-02-06 16:58:34 -0700

    added button

--- App.js
+++ App.js
@@ -1,27 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
 
 export default function App() {
+  const [clicked, setClicked] = useState(false)
+
+  const styles = StyleSheet.create({
+    container: {
+      // flex: 1,
+      backgroundColor: clicked ? 'orange' : 'wheat',
+      // alignItems: 'left',
+      justifyContent: 'center',
+      width: 300,
+      height: 300,
+      padding: 10,
+      // marginLeft: '10%'
+    },
+  });
+  
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <View style={styles.container}>
-        <Text>Hi, my name is Devin!</Text>
+        <Text style={{fontSize: 20}}>Hi, my name is Devin!</Text>
         <Text>This is my first React Native app!</Text>
-        <StatusBar style="auto" />
+        <Button title={clicked ? 'nice' : 'click me'} onPress={() => setClicked(true)} />
       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    backgroundColor: 'wheat',
-    // alignItems: 'left',
-    justifyContent: 'center',
-    width: 200,
-    height: 200,
-    padding: 10,
-    // marginLeft: '10%'
-  },
-});
