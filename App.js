fe95ca3fa9b0c427f19541672fa08a226a2bd429
devinrbopp <devinrbopp@gmail.com>
2022-02-06 16:23:24 -0700

    added nesting views

--- App.js
+++ App.js
@@ -3,20 +3,25 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hi, my name is Devin!</Text>
-      <Text>This is my first React Native app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={styles.container}>
+        <Text>Hi, my name is Devin!</Text>
+        <Text>This is my first React Native app!</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: 'wheat',
     // alignItems: 'left',
     justifyContent: 'center',
-    marginLeft: '10%'
+    width: 200,
+    height: 200,
+    padding: 10,
+    // marginLeft: '10%'
   },
 });
